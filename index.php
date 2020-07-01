22dbb3bbfd1bdd72f525cad82fd587cc54a877dd
Sahilramjali <35887583+Sahilramjali@users.noreply.github.com>
2020-07-01 12:08:36 +0545

    home page

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Education Sansar</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="javascript/main.js" defer></script>
+</head>
+<body>
+   <?php include('header.php');?><!--include header.php in this page-->
+   <div class="container-of-course">
+        <div class="course-container">
+            <a href="course.php"><img src="images/web21.jpg" >           
+           <div class="topic">
+               <h3 >Web Development</h3>
+           </div>
+            </a>
+        </div>
+        
+        <div class="course-container">
+        <a href="course.php"><img src="images/apps.jpg" >           
+           <div class="topic">
+               <h3 >App development</h3>
+           </div>
+           </a>
+        </div>
+        
+        <div class="course-container">
+        <a href="course.php"><img src="images/mores.png" >           
+           <div class="topic">
+               <h3 >more courses</h3>
+           </div>
+           </a>
+        </div>
+       
+       
+   </div>
+   <!--what people says portion-->
+
+   <div class="saying-title">
+   <h2>What Student Say</h2>
+   </div>
+   <div class="saying">
+   
+       <div class="student-saying">
+           <img src="images/say1.jpg"><span style="color:gray;">John</span>
+           <div class="student-feedback">
+               <p>"This website made me learn nothing to something in my career of web development. This website materials are the best material to learn."</P>
+            </div>
+
+        </div>
+        <div class="student-saying">
+           <img src="images/say1.jpg"><span style="color:gray;">John</span>
+           <div class="student-feedback">
+               <p>"Trust me guys,This website let u learn free and make you earn."</P>
+            </div>
+
+        </div>
+        <div class="student-saying">
+           <img src="images/say1.jpg"><span style="color:gray;">John</span>
+           <div class="student-feedback">
+               <p>"This website made me learn nothing to something in my career of web development. This website materials are the best material to learn."</P>
+            </div>
+
+        </div>
+
+
+    </div> 
+
+   <?php include('footer.php');?><!--include footer.php in this page-->
+</body>
+</html>
