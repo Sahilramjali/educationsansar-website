ab0e2dc5c8ed579662e8b91dd0971baf2ea68c1b
Sahilramjali <35887583+Sahilramjali@users.noreply.github.com>
2021-11-13 20:54:29 +0545

    added folders
    clean code practice

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+
+let toggleButtons = document.getElementsByClassName('toggle-button')[0];
+let navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+//listen event click sound in 3 bar icon
+toggleButtons.addEventListener('click',() => {
+    navbarLinks.classList.toggle('active');
+})
+
+
+//for popup message or modal 
+let modal=document.getElementById("modal");
+let modalbtn=document.getElementById("submit-feedback");    
+let closebtn=document.getElementById("close-btn");
+//listen event click sound
+modalbtn.addEventListener('click',openModal);
+closebtn.addEventListener('click',closeModal);
+window.addEventListener('click',outsideclick);
+function openModal(){
+   modal.style.display='block';
+}
+function closeModal(){
+    modal.style.display='none';
+}
+function outsideclick(e){
+   if(e.target==modal){
+       modal.style.display='none';
+    }
+}
